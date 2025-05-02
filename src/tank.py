0c97d00019c6f2f087e18d1ca6d52f695a5a872e
pedrolucas7i <pedrolucas.core7i@gmail.com>
2025-05-02 21:32:46 +0100

    Updated to use libregpio oficial le potato python3 library

--- src/tank.py
+++ src/tank.py
@@ -1,61 +1,39 @@
-import subprocess
+import time
 from threading import Lock
-from time import sleep
 import env
-
-"""
-Função	            GPIO    Chip_Line
-Esq. FWD	        29	    1_96
-Esq. BWD	        31	    1_97
-Dir. FWD	        37	    1_84
-Dir. BWD	        33	    1_85
-Ultrassom Trigger	35	    1_86
-Ultrassom Echo	    32	    1_95
-Servo 0	            36	    1_81
-Servo 1	            38	    1_82
-"""
-
-# === GPIO ===
-
-def gpio_set(chip_line, value):
-    subprocess.run(['lgpio', 'set', chip_line, str(value)], check=False)
-
-def gpio_pwm(chip_line, duty_percent):
-    duty = int(duty_percent * 10000)  # Convert from 0.0–1.0 to microseconds
-    subprocess.run(['lgpio', 'pwm', chip_line, '100000', str(duty)], check=False)
-
+from libregpio import OUT, IN
 
 # === MOTOR CONTROL ===
 
 class Motor:
     def __init__(self):
-        self.left_forward = '1_96'
-        self.left_backward = '1_97'
-        self.right_forward = '1_84'
-        self.right_backward = '1_85'
+        self.left_forward = OUT("GPIOX_17")   # Pin 29
+        self.left_backward = OUT("GPIOX_18")  # Pin 31
+        self.right_forward = OUT("GPIOX_2")   # Pin 37
+        self.right_backward = OUT("GPIOX_3")  # Pin 33
         self.lock = Lock()
 
         for pin in [self.left_forward, self.left_backward, self.right_forward, self.right_backward]:
-            gpio_set(pin, 0)
+            pin.write(0)
 
     def duty_range(self, duty1, duty2):
         return max(-4095, min(4095, duty1)), max(-4095, min(4095, duty2))
 
     def left_Wheel(self, duty):
-        gpio_set(self.left_forward, 0)
-        gpio_set(self.left_backward, 0)
+        self.left_forward.write(0)
+        self.left_backward.write(0)
         if duty > 0:
-            gpio_set(self.left_forward, 1)
+            self.left_forward.write(1)
         elif duty < 0:
-            gpio_set(self.left_backward, 1)
+            self.left_backward.write(1)
 
     def right_Wheel(self, duty):
-        gpio_set(self.right_forward, 0)
-        gpio_set(self.right_backward, 0)
+        self.right_forward.write(0)
+        self.right_backward.write(0)
         if duty > 0:
-            gpio_set(self.right_forward, 1)
+            self.right_forward.write(1)
         elif duty < 0:
-            gpio_set(self.right_backward, 1)
+            self.right_backward.write(1)
 
     def setMotorModel(self, duty1, duty2):
         with self.lock:
@@ -89,78 +67,67 @@ class Motor:
     def close(self):
         self.stop()
 
-
 # === ULTRASONIC SENSOR ===
 
 class Ultrasonic:
     def __init__(self):
-        self.trigger = '1_86'  # GPIO 35
-        self.echo = '1_95'     # GPIO 32
-        gpio_set(self.trigger, 0)
+        self.trigger = OUT("GPIOX_19")  # Pin 35
+        self.echo = IN("GPIOX_16")      # Pin 32
+        self.trigger.write(0)
 
     def get_distance(self):
         import time
-        import lgpio
-        h = lgpio.gpiochip_open(1)
-        TRIG = 86
-        ECHO = 95
-        lgpio.gpio_claim_output(h, TRIG, 0)
-        lgpio.gpio_claim_input(h, ECHO)
-        
-        lgpio.gpio_write(h, TRIG, 1)
+        self.trigger.write(1)
         time.sleep(0.00001)
-        lgpio.gpio_write(h, TRIG, 0)
+        self.trigger.write(0)
 
         start = time.time()
-        while lgpio.gpio_read(h, ECHO) == 0:
+        while self.echo.read() == 0:
             start = time.time()
-        while lgpio.gpio_read(h, ECHO) == 1:
+        while self.echo.read() == 1:
             stop = time.time()
 
         elapsed = stop - start
         distance = (elapsed * 34300) / 2  # cm
-        lgpio.gpiochip_close(h)
         return round(distance, 1)
 
     def close(self):
         pass
 
-
 # === SERVO CONTROL ===
 
 class Clamp:
     def __init__(self):
-        self.servo0 = '1_81'  # GPIO 36
-        self.servo1 = '1_82'  # GPIO 38
+        self.servo0 = OUT("GPIOX_13")  # Pin 36
+        self.servo1 = OUT("GPIOX_14")  # Pin 38
 
     def up(self):
         for i in range(180, 90, -1):
             val = (i - 90) / 90
-            gpio_pwm(self.servo1, val)
-            sleep(0.01)
+            self.servo1.write(val)
+            time.sleep(0.01)
         for i in range(170, 90, -1):
             val = (i - 90) / 90
-            gpio_pwm(self.servo0, val)
-            sleep(0.01)
+            self.servo0.write(val)
+            time.sleep(0.01)
         for i in range(90, 180, 1):
             val = (i - 90) / 90
-            gpio_pwm(self.servo1, val)
-            sleep(0.01)
+            self.servo1.write(val)
+            time.sleep(0.01)
 
     def down(self):
         for i in range(180, 90, -1):
             val = (i - 90) / 90
-            gpio_pwm(self.servo1, val)
-            sleep(0.01)
+            self.servo1.write(val)
+            time.sleep(0.01)
         for i in range(90, 170, 1):
             val = (i - 90) / 90
-            gpio_pwm(self.servo0, val)
-            sleep(0.01)
+            self.servo0.write(val)
+            time.sleep(0.01)
         for i in range(90, 180, 1):
             val = (i - 90) / 90
-            gpio_pwm(self.servo1, val)
-            sleep(0.01)
-
+            self.servo1.write(val)
+            time.sleep(0.01)
 
 # === MAIN ===
 
@@ -170,9 +137,9 @@ if __name__ == '__main__':
     try:
         while True:
             motor.driveLeft(4)
-            sleep(0.35)
+            time.sleep(0.35)
             motor.driveForward(1)
-            sleep(0.5)
+            time.sleep(0.5)
     except KeyboardInterrupt:
         motor.stop()
         motor.close()
