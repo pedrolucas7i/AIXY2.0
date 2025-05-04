f97290bd77915124c720b4d26cdd32318eebd6a7
pedrolucas7i <pedrolucas.core7i@gmail.com>
2025-05-04 13:58:41 +0100

    Fixed

--- src/hardware.py
+++ src/hardware.py
@@ -30,27 +30,27 @@ except Exception as e:
 
 # === MOTOR CONTROL ===
 def drive_forward():
-    ser.write("drive_forward\n")
+    ser.write("drive_forward\n".encode())
 
 def drive_backward():
-    ser.write("drive_backward\n")
+    ser.write("drive_backward\n".encode())
 
 def drive_left():
-    ser.write("drive_left\n")
+    ser.write("drive_left\n".encode())
 
 def drive_right():
-    ser.write("drive_right\n")
+    ser.write("drive_right\n".encode())
 
 def drive_release():
-    ser.write("drive_release\n")
+    ser.write("drive_release\n".encode())
 
 def drive_stop():
-    ser.write("drive_stop\n")
+    ser.write("drive_stop\n".encode())
 
 
 # === ULTRASONIC SENSOR ===
 def get_distance():
-    ser.write("ultrassonic_data\n")
+    ser.write("ultrassonic_data\n".encode())
     response = ser.readline().decode('utf-8').strip()
     try:
         return float(response)
@@ -61,11 +61,11 @@ def get_distance():
 
 # === SERVO CONTROL ===
 def clamp_catch():
-    ser.write("arm_down\n")
-    ser.write("clamp_catch\n")
-    ser.write("arm_up\n")
+    ser.write("arm_down\n".encode())
+    ser.write("clamp_catch\n".encode())
+    ser.write("arm_up\n".encode())
 
 def clamp_release():
-    ser.write("arm_down\n")
-    ser.write("clamp_release\n")
-    ser.write("arm_up\n")
+    ser.write("arm_down\n".encode())
+    ser.write("clamp_release\n".encode())
+    ser.write("arm_up\n".encode())
