d8aa912ec869edc5e084f27e14f2079019b79903
pedrolucas7i <pedrolucas.core7i@gmail.com>
2025-05-09 21:14:21 +0100

    Trying to fiz thing

--- src/aixy.py
+++ src/aixy.py
@@ -377,7 +377,53 @@ def main():
 
     if env.WCS:
         print("🟢 Starting Web Camera Stream thread...")
-        WCS_PROCESSOR = threading.Thread(target=WCS_thread.run, daemon=True)
-        WCS_PROCESSOR.start()
+        from flask import Flask, render_template, Response
+        import env
+
+        if env.CAMERA:
+            from camera import CameraUSB
+            camera = CameraUSB()
+
+        if env.MOTORS:
+            import hardware
+
+
+        #Initialize the Flask app
+        app = Flask(__name__, template_folder="./WCS_thread/webserver", static_folder="./WCS_thread/static")
+
+        @app.route('/')
+        def index():
+            return render_template('index.html')
+
+        if env.MOTORS:
+            @app.route('/forward')
+            def forward():
+                hardware.drive_forward()
+
+            @app.route('/left')
+            def left():
+                hardware.drive_left()
+
+            @app.route('/right')
+            def right():
+                hardware.drive_right()
+
+            @app.route('/backward')
+            def backward():
+                hardware.drive_backward()
+
+            @app.route('/release')
+            def release():
+                hardware.drive_release()
+
+        if env.CAMERA:
+            @app.route('/stream')
+            def stream():
+                return Response(camera.get_web_stream(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+        def run():
+            app.run(debug=True, port=9900, host="0.0.0.0")
+
+        run()
 
 main()
