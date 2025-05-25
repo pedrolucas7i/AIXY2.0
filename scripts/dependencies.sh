e9053435383193b30d4b23d7d668d0194ec20a81
pedrolucas7i <pedrolucas.core7i@gmail.com>
2025-05-25 12:39:19 +0100

    Refactor dependencies installation script for improved readability and consistency; ensure all apt commands use the -y flag and non root user

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -14,19 +14,12 @@ echo """
 """
 
 
-# Must be run as root
-if [ "$EUID" -ne 0 ]; then
-  echo "Please run this script as root (use sudo)."
-  exit 1
-fi
-
-
-sudo apt update
-sudo apt install libportaudio2 libportaudiocpp0 portaudio19-dev 
-sudo apt install git mpg123
+sudo apt update -y
+sudo apt install -y libportaudio2 libportaudiocpp0 portaudio19-dev 
+sudo apt install -y git mpg123
 sudo apt install -y pulseaudio pulseaudio-module-bluetooth pavucontrol bluez blueman
-sudo apt install python3-pip python3-pygame python3-flask
-pip3 install pyserial opencv-python sounddevice gtts playsound ollama flask_socketio edge-tts--break-system-packages
+sudo apt install -y python3-pip python3-pygame python3-flask
+pip3 install pyserial opencv-python sounddevice gtts playsound ollama flask_socketio edge-tts --break-system-packages
 
 
 echo """
